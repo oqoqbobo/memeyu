7cb8c6328478ed21f8863378d5754e60d7fb749d
oqoqbobo <156905832@qq.com>
2022-08-09 23:54:38 +0800

    test

---
+++ fuck/src/main/java/com/lajimmy/fuck/FuckApplication.java
@@ -0,0 +1,13 @@
+package com.lajimmy.fuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuckApplication.class, args);
+    }
+
+}
---
+++ fuck/src/test/java/com/lajimmy/fuck/FuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lajimmy.fuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
